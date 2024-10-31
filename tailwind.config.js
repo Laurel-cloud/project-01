3c641e1ad707f5465f7b5e1340090f33f911019e
Gyesu <dwlsc0304@gmail.com>
2024-10-31 11:38:58 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // 이 부분에 프로젝트 파일 경로를 지정해 주세요
+  ],  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
